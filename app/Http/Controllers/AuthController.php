a242550d790ec0b346bb8d81ff9b01d9e740dba3
sohaib ek khatab <sohaib.el.khatab@gmail.com>
2025-01-21 15:52:48 +0100

    adding role field with subscriber as default value in user table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,6 +33,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'subscriber',
         ]);
 
         Auth::login($user);
