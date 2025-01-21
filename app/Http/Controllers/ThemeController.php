567f3cf2261a644ea59743df04cba2402cd24fed
sohaib ek khatab <sohaib.el.khatab@gmail.com>
2025-01-21 16:40:33 +0100

    testing theme controller

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 
 class ThemeController extends Controller
 {
-    public function index() {
-        
+    public function index()
+    {
+        $themes = DB::table('themes')->get();
     }
 }
