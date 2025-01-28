c4b19d76ce17642b7631ffb7fdbdf09e5285a42d
sohaib ek khatab <sohaib.el.khatab@gmail.com>
2025-01-28 14:17:08 +0100

    changing some things in css (auth and articles and article details)

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -27,12 +27,6 @@
             <form action="{{ route('register') }}" method="POST">
                 @csrf
                 <h1>Create Account</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-                <span>or use your email for registration</span>
                 <input type="text" name="name" placeholder="Name" value="{{ old('name') }}" required />
                 @error('name')
                     <span class="error">{{ $message }}</span>
@@ -53,12 +47,6 @@
             <form action="{{ route('login') }}" method="POST">
                 @csrf
                 <h1>Sign in</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-                <span>or use your account</span>
                 <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" required />
                 <input type="password" name="password" placeholder="Password" required />
                 <a href="#" id="forgot">Forgot your password?</a>
