d6acdf0c9b8de943d6eea2ec8d10cb20330cd577
sohaib ek khatab <sohaib.el.khatab@gmail.com>
2025-01-28 01:11:09 +0100

    changing a bit in css

--- app/Models/ProposedArticle.php
+++ app/Models/ProposedArticle.php
@@ -17,16 +17,14 @@ class ProposedArticle extends Model
         'theme_id',
         'author_id',
         'ispublic',
-        'position', // Add this line
+        'position',
     ];
 
-    // Define a relationship to fetch the author (user)
     public function author()
     {
         return $this->belongsTo(User::class, 'author_id');
     }
 
-    // Define a relationship to fetch the theme
     public function theme()
     {
         return $this->belongsTo(Theme::class, 'theme_id');
